b0f2fa00e8b22a513610a304ecc5fdf3fd673f5e
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-21 02:47:40 +0300

    a script that reads stdin line by line and computes metrics:
    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> (if the format is not this one, the line must be skipped)
    After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
    Total file size: File size: <total size>
    where <total size> is the sum of all previous <file size> (see input format above)
    Number of lines by status code:
    possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
    if a status code doesn’t appear or is not an integer, don’t print anything for this status code
    format: <status code>: <number>
    status codes should be printed in ascending order
    Warning: In this sample, you will have random value - it’s normal to not have the same output as this one.

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Log parsing"""
+import sys
+
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+total_size = 0
+counter = 0
+
+try:
+    for line in sys.stdin:
+        counter += 1
+        data = line.split()
+        try:
+            total_size += int(data[-1])
+        except:
+            pass
+        try:
+            if data[-2] in status_codes:
+                status_codes[data[-2]] += 1
+        except:
+            pass
+        if counter == 10:
+            print("File size: {}".format(total_size))
+            for key, value in sorted(status_codes.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
+            counter = 0
+except KeyboardInterrupt:
+    pass
+finally:
+    print("File size: {}".format(total_size))
+    for key, value in sorted(status_codes.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
