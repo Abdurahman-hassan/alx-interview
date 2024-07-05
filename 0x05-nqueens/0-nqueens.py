e5ead608f67adfa8112affd2c10b93d2fec7431a
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-05 03:05:26 +0300

    enhance print statment

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -31,9 +31,9 @@ if __name__ == "__main__":
     if not sys.argv[1].isdigit():
         print("N must be a number")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
+    N = int(sys.argv[1])
+    if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    board = [-1 for i in range(int(sys.argv[1]))]
+    board = [-1 for i in range(N)]
     solveNQ(board, 0)
