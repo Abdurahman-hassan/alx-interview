863853a7152b5a78a98a6f242ff46dae8a7ff3b2
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-12 02:55:40 +0300

    fix linting

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,17 +2,17 @@
 const request = require('request');
 
 function fetchCharacter (urls) {
-    if (urls.length === 0) return;
-    request.get(urls[0], (_error, _response, body) => {
-        console.log(JSON.parse(body).name);
-        fetchCharacter(urls.slice(1));
-    });
+  if (urls.length === 0) return;
+  request.get(urls[0], (_error, _response, body) => {
+    console.log(JSON.parse(body).name);
+    fetchCharacter(urls.slice(1));
+  });
 }
 
 request.get(
-    'https://swapi-api.alx-tools.com/api/films/' + process.argv[2],
-    (_error, _response, body) => {
-        const characters = JSON.parse(body).characters;
-        fetchCharacter(characters);
-    }
+  'https://swapi-api.alx-tools.com/api/films/' + process.argv[2],
+  (_error, _response, body) => {
+    const characters = JSON.parse(body).characters;
+    fetchCharacter(characters);
+  }
 );
