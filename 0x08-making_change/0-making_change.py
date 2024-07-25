b071afbbbeee055352216d3437a6ab209b2578fa
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-07-25 23:37:46 +0300

    Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
    Prototype: def makeChange(coins, total)
    Return: fewest number of coins needed to meet total
    If total is 0 or less, return 0
    If total cannot be met by any number of coins you have, return -1
    coins is a list of the values of the coins in your possession
    The value of a coin will always be an integer greater than 0
    You can assume you have an infinite number of each denomination of coin in the list
    Your solution’s runtime will be evaluated in this task

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""makeChange module"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest number of coins
+    needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    n = len(coins)
+    i = 0
+    count = 0
+    while i < n and total > 0:
+        if coins[i] <= total:
+            total -= coins[i]
+            count += 1
+        else:
+            i += 1
+    if total == 0:
+        return count
+    return -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,3 @@
+# This is a 0x08-making_change
+[]: # Compare this snippet from 0x07-rotate_2d_matrix/README.md:
+[]: # ## 0x07. Rotate 2D Matrix
